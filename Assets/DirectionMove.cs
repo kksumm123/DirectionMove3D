1c0c519f9519113badd288c27dc5cb58dd348404
kksumm123 <kksumm123@gmail.com>
2021-06-21 19:26:16 +0900

    꽤나 수정함

--- Assets/DirectionMove.cs
+++ Assets/DirectionMove.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,51 +24,74 @@ public class DirectionMove : MonoBehaviour
         Idle,
         Run,
         Jump,
-        Attack
+        Attack,
+        None
     }
     void Start()
     {
         animator = GetComponent<Animator>();
-
     }
+    Dictionary<string, float> animationLength = null;
     void Update()
     {
         Move();
-
-        if(Input.GetMouseButtonDown(0))
-        {
-            State = StateType.Attack;
-            animator.Play("Attack", 1);
-        }
+        Attack();
     }
+
 
     Vector3 move;
     private void Move()
     {
-        move = Vector3.zero;
-        if (Input.GetKey(KeyCode.A))
-            move.x = -1;
-        if (Input.GetKey(KeyCode.D))
-            move.x = 1;
-        if (Input.GetKey(KeyCode.W))
-            move.z = 1;
-        if (Input.GetKey(KeyCode.S))
-            move.z = -1;
-        if (move != Vector3.zero)
+        if (State != StateType.Attack)
         {
-            State = StateType.Run;
-            animator.Play("Run");
-            move.Normalize();
-            transform.Translate(move * speed * Time.deltaTime, Space.World);
+            move = Vector3.zero;
+            if (Input.GetKey(KeyCode.A))
+                move.x = -1;
+            if (Input.GetKey(KeyCode.D))
+                move.x = 1;
+            if (Input.GetKey(KeyCode.W))
+                move.z = 1;
+            if (Input.GetKey(KeyCode.S))
+                move.z = -1;
+            if (move != Vector3.zero)
+            {
+                State = StateType.Run;
+                animator.Play("Run");
+                move.Normalize();
+                transform.Translate(move * speed * Time.deltaTime, Space.World);
 
-            lastMoveDir = move;
+                lastMoveDir = move;
+            }
+            else
+            {
+                State = StateType.Idle;
+                animator.Play("Idle");
+            }
         }
-        else
-        {
-            State = StateType.Idle;
-            animator.Play("Idle");
-        }
+
         transform.forward = Vector3.Slerp(transform.forward, lastMoveDir, rotateLerp);
+    }
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 아래처럼 레이어 + 블랜더 사용하면 애니메이션이 섞이지만 
+            // animator.Play("Attack", 1);
+            // 이번에는 
+            // 어택을 했으면 어택 애니메이션 진행중인 동안
+            // Run과 Idle 안되게 하자
+            // 각 애니메이션의 길이가 필요
+            // 어택이 끝나면 State = None
+            StartCoroutine(AttackCo());
+        }
+        IEnumerator AttackCo()
+        {
+            State = StateType.Attack;
+            animator.Play("Attack");
+            float attackAnimationTime = animationLength["Attack"];
+            yield return new WaitForSeconds(attackAnimationTime);
+            State = StateType.None;
+        }
     }
 
 }
