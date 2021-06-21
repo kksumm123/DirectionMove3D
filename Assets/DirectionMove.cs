ad67e3e7436882fa60914db9476854bb97e91b21
kksumm123 <kksumm123@gmail.com>
2021-06-21 17:16:08 +0900

    움직임 구현 완료

--- Assets/DirectionMove.cs
+++ Assets/DirectionMove.cs
@@ -1,18 +1,30 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DirectionMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator;
+    [SerializeField] float speed = 5f;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+            move.x = -1;
+        if (Input.GetKey(KeyCode.D))
+            move.x = 1;
+        if (Input.GetKey(KeyCode.W))
+            move.z = 1;
+        if (Input.GetKey(KeyCode.S))
+            move.z = -1;
+        move.Normalize();
+        transform.Translate(move * speed * Time.deltaTime, Space.World);
+
     }
 }
